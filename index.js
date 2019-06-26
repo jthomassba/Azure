cc36de59a2bfa992c7eae6c4edd72da35171fc86
jthomassba <52168645+jthomassba@users.noreply.github.com>
2019-06-26 09:26:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World from Azure Pipeline!");
+    response.end("Hello World from Azure Pipeline to SBA!");
 });
 
 const port = process.env.PORT || 1337;
